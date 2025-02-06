4f6527b0d6a1fd427d20049f0b01d2dcef072a50
SAIHAYA73 <saihaya2243@icloud.com>
2025-02-06 12:36:29 +0900

    tmp4

--- Assets/Scripts/TestScene/TypingGame.cs
+++ Assets/Scripts/TestScene/TypingGame.cs
@@ -14,6 +14,62 @@ public class TypingGame : MonoBehaviour
     public List<string> wordList;
     public float gameTime = 60f;
 
+    private Dictionary<string, int> fingerPosition = new Dictionary<string, int>()
+    {
+        { "Q", 1 },
+        { "q", 1 },
+        { "A", 1 },
+        { "a", 1 },
+        { "Z", 1 },
+        { "z", 1 },
+        { "W", 2 },
+        { "w", 2 },
+        { "S", 2 },
+        { "s", 2 },
+        { "X", 2 },
+        { "x", 2 },
+        { "E", 3 },
+        { "e", 3 },
+        { "D", 3 },
+        { "d", 3 },
+        { "C", 3 },
+        { "c", 3 },
+        { "R", 4 },
+        { "r", 4 },
+        { "F", 4 },
+        { "f", 4 },
+        { "V", 4 },
+        { "v", 4 },
+        { "T", 4 },
+        { "t", 4 },
+        { "G", 4 },
+        { "g", 4 },
+        { "B", 4 },
+        { "b", 4 },
+        { "Y", 5 },
+        { "y", 5 },
+        { "H", 5 },
+        { "h", 5 },
+        { "N", 5 },
+        { "n", 5 },
+        { "U", 5 },
+        { "u", 5 },
+        { "J", 5 },
+        { "j", 5 },
+        { "M", 5 },
+        { "m", 5 },
+        { "I", 6 },
+        { "i", 6 },
+        { "K", 6 },
+        { "k", 6 },
+        { "O", 7 },
+        { "o", 7 },
+        { "L", 7 },
+        { "l", 7 },
+        { "P", 8 },
+        { "p", 8 }
+    };
+
     private string currentWord;
     private int score = 0;
     private float timeRemaining;
