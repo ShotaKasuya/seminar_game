5b442a955f2d176a4ff5f136aafd8c882d20b961
SAIHAYA73 <saihaya2243@icloud.com>
2025-02-08 14:03:21 +0900

    timp

--- Assets/Scripts/Domain/UseCase/InGame/MissTypeCase.cs
+++ Assets/Scripts/Domain/UseCase/InGame/MissTypeCase.cs
@@ -1,7 +1,22 @@
+using Domain.IModel.InGame;
+
 namespace Domain.UseCase.InGame
 {
     public class MissTypeCase
     {
+        public MissTypeCase
+        (
+            IFailEventModel onFailEvent
+        )
+        {
+            FailEventModel = onFailEvent;
+        }
+
+        private void OnFailEvent()
+        {
+            
+        }
         
+        private IFailEventModel FailEventModel { get; }
     }
 }
