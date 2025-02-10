0eabb0b6753ece5879990c4f991d10e24ea4bf7e
SAIHAYA73 <saihaya2243@icloud.com>
2025-02-10 14:02:20 +0900

    改修

--- Assets/Scripts/Installer/InGame/TypingGameInstaller.cs
+++ Assets/Scripts/Installer/InGame/TypingGameInstaller.cs
@@ -3,17 +3,25 @@ using Model.InGame;
 using Module.Installer;
 using Module.Serial;
 using UnityEngine;
+using View.InGame;
 
 namespace Installer.InGame
 {
         public class TypingGameInstaller:InstallerBase
     {
         [SerializeField] GameSettingModel gameSetting;
+        [SerializeField] UiElementView uiElementView;
         protected override void CustomConfigure()
         {
             var questionResultEventModel = new QuestionResultEventModel();
             var port = GlobalLocator.Instance.Resolve<IPortWritable>();
             var missTypeCase = new MissTypeCase(questionResultEventModel, port, gameSetting);
+            var questionModel = new QuestionModel();
+            var judgeTypeCase = new JudgeTypeCase(questionModel, questionModel, questionResultEventModel, uiElementView);
+            var completeTypeCase = new CompleteTypeCase(questionResultEventModel, gameSetting, questionModel);
+            var timerModel = new TimerModel();
+            var timeUpdateCase = new TimeUpdateCase(timerModel, uiElementView);
+            
         }
     }
 }
